5acc3d510f80a6dc526408131eb2e7cbf64b430a
Ryan Garofano <ryan@ryangarofano.ca>
2025-09-14 11:43:51 -0700

    Lazy load rose-pine theme

--- lua/plugins/omarchy-theme.lua
+++ lua/plugins/omarchy-theme.lua
@@ -81,6 +81,7 @@ return {
     {
         "rose-pine/neovim",
         name = "rose-pine",
+        lazy = true,
         config = function()
             require("rose-pine").setup({
                 variant = "dawn",
