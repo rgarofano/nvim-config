1433ddfaead648dd5b29b65ad7f88bef1d1289be
Ryan Garofano <ryan@ryangarofano.ca>
2025-10-18 15:15:41 -0700

    Change from super tab to default C-y to accept completions

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -29,6 +29,9 @@ return {
         lspconfig.ts_ls.setup({
             capabilities = capabilities,
         })
+        lspconfig.jedi_language_server.setup({
+            capabilities = capabilities,
+        })
         vim.keymap.set("n", "K", vim.lsp.buf.hover)
         vim.keymap.set("n", "gd", vim.lsp.buf.definition)
         vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
