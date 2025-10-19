a11d8408d7e49122913be739fbe3354556f7a1a9
Ryan Garofano <ryan@ryangarofano.ca>
2025-10-19 12:23:25 -0700

    Cycle through quick fix list with C-n and C-p

--- lua/buffers/init.lua
+++ lua/buffers/init.lua
@@ -7,9 +7,8 @@ vim.keymap.set("n", "<BS>", "<cmd>e #<Enter>")
 vim.keymap.set("n", "<Tab>", "<cmd>bnext<Enter>")
 vim.keymap.set("n", "<S-Tab>", "<cmd>bprevious<Enter>")
 -- Quick fix lists
-vim.keymap.set("n", "<C-q>", "<C-q><cmd>cclose<Enter>", { noremap = true })
 vim.keymap.set("n", "<C-n>", "<cmd>cnext<Enter>zz")
-vim.keymap.set("n", "<C-N>", "<cmd>cprevious<Enter>zz")
+vim.keymap.set("n", "<C-p>", "<cmd>cprevious<Enter>zz")
 -- Better page jumping
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true })
