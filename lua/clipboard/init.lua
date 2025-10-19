754a5484bc6a9a350623862b4146de8ab8afab1c
Ryan Garofano <ryan@ryangarofano.ca>
2025-10-19 15:13:51 -0700

    Copy with Ctrl+C and Ctrl+V only

--- lua/clipboard/init.lua
+++ lua/clipboard/init.lua
@@ -1,2 +1,5 @@
-vim.keymap.set("n", "<leader>y", '"+y')
-vim.keymap.set("v", "<leader>y", '"+y')
+-- Copying to system clipboard
+vim.keymap.set("v", "<C-c>", '"+y')
+-- Pasting from system clipboard
+vim.keymap.set("i", "<C-v>", "<C-r>+")
+vim.keymap.set('t', '<C-v>', [[<C-\><C-n>"+pi]])
