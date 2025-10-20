339f9b52811b08c5baba5fa54982448b6ff6a50a
Ryan Garofano <ryan@ryangarofano.ca>
2025-10-19 20:04:42 -0700

    Add format on save through lsp and add keymap for putting diagnostics into quick fix list

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -34,9 +34,26 @@ return {
         })
         vim.keymap.set("n", "K", vim.lsp.buf.hover)
         vim.keymap.set("n", "gd", vim.lsp.buf.definition)
-        vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
         vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
         -- enable inline diagnostics
         vim.diagnostic.config({ virtual_text = true })
+        vim.keymap.set("n", "<leader>qd", vim.diagnostic.setqflist)
+
+        vim.api.nvim_create_autocmd("LspAttach", {
+            group = vim.api.nvim_create_augroup("lsp.format", {}),
+            callback = function(args)
+                local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+                if not client:supports_method("textDocument/willSaveWaitUntil")
+                    and client:supports_method("textDocument/formatting") then
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        group = vim.api.nvim_create_augroup("lsp.format", { clear = false }),
+                        buffer = args.buf,
+                        callback = function()
+                            vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+                        end,
+                    })
+                end
+            end
+        })
     end,
 }
