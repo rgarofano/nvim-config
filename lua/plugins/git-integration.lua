5f347e80e9173d8d21f7f4f5fd5a1da9a6776247
Ryan Garofano <ryan@ryangarofano.ca>
2025-10-20 09:51:57 -0700

    Update git integration keymaps

--- lua/plugins/git-integration.lua
+++ lua/plugins/git-integration.lua
@@ -1,15 +1,11 @@
 return {
-    {
-        "tpope/vim-fugitive",
-    },
-
-    {
-        "lewis6991/gitsigns.nvim",
-        config = function()
-            require("gitsigns").setup()
-            vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-            vim.keymap.set("n", "<leader>gn", ":Gitsigns next_hunk<CR>", {})
-            vim.keymap.set("n", "<leader>gb", ":Gitsigns blame_line<CR>", {})
-        end,
-    },
+    "lewis6991/gitsigns.nvim",
+    config = function()
+        require("gitsigns").setup()
+        vim.keymap.set("n", "gK", "<cmd>Gitsigns preview_hunk<Enter>")
+        vim.keymap.set("n", "<C-g>", "<cmd>Gitsigns next_hunk<Enter>")
+        vim.keymap.set("n", "<C-S-g>", "<cmd>Gitsigns prev_hunk<Enter>")
+        vim.keymap.set("n", "<leader>gb", "<cmd>Gitsigns blame_line<Enter>")
+        vim.keymap.set("n", "<leader>gu", "<cmd>Gitsigns reset_hunk<Enter>")
+    end,
 }
