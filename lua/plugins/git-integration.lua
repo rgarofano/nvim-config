42af3d508dd9eafae9b690f5d3d8b19499c09fe5
Ryan Garofano <ryan@ryangarofano.ca>
2025-11-27 20:49:39 -0800

    Change all gitsigns keybindings to use leader key

--- lua/plugins/git-integration.lua
+++ lua/plugins/git-integration.lua
@@ -2,9 +2,9 @@ return {
     "lewis6991/gitsigns.nvim",
     config = function()
         require("gitsigns").setup()
-        vim.keymap.set("n", "gK", "<cmd>Gitsigns preview_hunk<Enter>")
-        vim.keymap.set("n", "<C-g>", "<cmd>Gitsigns next_hunk<Enter>")
-        vim.keymap.set("n", "<C-S-g>", "<cmd>Gitsigns prev_hunk<Enter>")
+        vim.keymap.set("n", "<leader>gp", "<cmd>Gitsigns preview_hunk<Enter>")
+        vim.keymap.set("n", "<leader>nh", "<cmd>Gitsigns next_hunk<Enter>")
+        vim.keymap.set("n", "<leader>ph", "<cmd>Gitsigns prev_hunk<Enter>")
         vim.keymap.set("n", "<leader>gb", "<cmd>Gitsigns blame_line<Enter>")
         vim.keymap.set("n", "<leader>gu", "<cmd>Gitsigns reset_hunk<Enter>")
     end,
