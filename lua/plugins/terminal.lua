4fdc70ffa42376cf0318fb11bf0888c63c65e2d9
Ryan Garofano <ryan@ryangarofano.ca>
2025-11-29 17:18:25 -0800

    Switch to plugin for floating terminal

---
+++ lua/plugins/terminal.lua
@@ -0,0 +1,23 @@
+return {
+    "akinsho/toggleterm.nvim",
+    config = function()
+        require("toggleterm").setup()
+        vim.keymap.set({ "n", "t" }, "\\f", "<cmd>ToggleTerm direction=float<Enter>")
+        -- Easily switch to normal mode for copying, scrolling, etc.
+        vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
+        -- Open terminal in current window
+        vim.keymap.set("n", "\\t", "<cmd>terminal<Enter>")
+        -- Open terminal in horizontal split
+        vim.keymap.set({ "n", "t" }, "\\s", function()
+            vim.cmd.split()
+            vim.cmd.terminal()
+            vim.cmd("startinsert")
+        end)
+        -- Open terminal in vertical split
+        vim.keymap.set({ "n", "t" }, "\\v", function()
+            vim.cmd.vsplit()
+            vim.cmd.terminal()
+            vim.cmd("startinsert")
+        end)
+    end
+}
