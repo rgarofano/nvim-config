e6ddc3d1f2535642a2075128f5879448e4f125cf
Ryan Garofano <ryan@ryangarofano.ca>
2025-12-25 18:06:05 -0800

    Change terminal keybinds

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -2,22 +2,22 @@ return {
     "akinsho/toggleterm.nvim",
     config = function()
         require("toggleterm").setup()
-        vim.keymap.set({ "n", "t" }, "\\f", "<cmd>ToggleTerm direction=float<Enter>")
+        vim.keymap.set({ "n", "t" }, "<M-Esc>", "<cmd>ToggleTerm direction=float<Enter>")
         -- Easily switch to normal mode for copying, scrolling, etc.
         vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>")
         -- Open terminal in current window
-        vim.keymap.set("n", "\\t", function()
+        vim.keymap.set("n", "<leader>tt", function()
             vim.cmd.terminal()
             vim.cmd("startinsert")
         end)
         -- Open terminal in horizontal split
-        vim.keymap.set({ "n", "t" }, "\\s", function()
+        vim.keymap.set({ "n", "t" }, "<M-s>", function()
             vim.cmd.split()
             vim.cmd.terminal()
             vim.cmd("startinsert")
         end)
         -- Open terminal in vertical split
-        vim.keymap.set({ "n", "t" }, "\\v", function()
+        vim.keymap.set({ "n", "t" }, "<M-v>", function()
             vim.cmd.vsplit()
             vim.cmd.terminal()
             vim.cmd("startinsert")
